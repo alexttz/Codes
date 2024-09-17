0d9235b61e07dfc79ccac80ddec595bb9875cffe
Alexander Silva Lima <lima.alexander.las@gmail.com>
2024-09-17 16:26:21 -0300

    create window in Java JFrame

---
+++ Java/WindowJava/Window/src/Janela.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Janela {
+    public static void main(String[] args) throws Exception {
+        JFrame janela = new JFrame();
+        janela.setTitle("Minha primeira janela! ");
+        janela.setSize(500, 500);
+        janela.setVisible(true);
+
+        //Criando uma janela onde seu tamanho é de 500 x 500 com o título "Minha primeira janela!", para isso importamos a bliblioteca javax.swing.JFrame onde temos as ferramentas necessárias para criar as telas.
+    }
+}
